2c1e8d9bac88d2b2bfd0146f0ac1d309fb75fd60
Yohwan Lim <dyghks7102@naver.com>
2023-04-01 16:30:17 +0900

    auto configuration test

--- autoconfig/src/main/java/hello/config/DbConfig.java
+++ autoconfig/src/main/java/hello/config/DbConfig.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.TransactionManager;
 import javax.sql.DataSource;
 
 @Slf4j
-@Configuration
+//@Configuration //spring-boot-autoconfigure
 public class DbConfig {
 
     @Bean
